e7842380df14412d88f7e2cacd1443f5be1ffa03
SUNKU BHARATH <94451263+Sunku-Venkateswarlu-Bharath@users.noreply.github.com>
2023-07-17 15:59:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Pagination from "./Pagination";
+import Search from "./Search";
+import Stories from "./Stories";
+import "./App.css";
+
+
+const App = () => {
+  return <>
+    <div>
+      </div>
+      <Search/>
+      <Pagination/>
+      <Stories/>
+  
+  </>
+};
+
+export default App;
+
